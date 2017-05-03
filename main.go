aa55769617c5a76967f3461b5d8cb7d56089311d
Abhishek Pradhan <Abhishek_Pradhan@external.mckinsey.com>
2017-05-03 15:28:33 +0530

    Add basic models for telegram

--- main.go
+++ main.go
@@ -1,34 +1,59 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
-	"github.com/julienschmidt/httprouter"
+	"io/ioutil"
 	"log"
 	"net/http"
 	"os"
+
+	"github.com/abhsk/AskMeAnything/telegram"
+
+	"github.com/gorilla/mux"
 )
 
-func indexHandler(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
-	fmt.Fprintf(w, "Welcome to AskMeAnything")
-}
+const (
+	BASEURL = "https://api.telegram.org/bot"
+	APIKEY  = "351267960:AAFfApJepRNqFGGKaUcd30xkWoGFD2I118E"
+)
 
-func messageHandler(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
-	//"message":{"message_id":271,"from":{"id":35364348,"first_name":"Abhishek","last_name":"Pradhan"},
+func IndexHandler() http.HandlerFunc {
+	//"message":{
+	// "message_id":271,"from":{"id":35364348,"first_name":"Abhishek","last_name":"Pradhan"},
 	// "chat":{"id":35364348,"first_name":"Abhishek","last_name":"Pradhan","type":"private"},
 	// "date":1493803385,"text":"/ping","entities":[{"type":"bot_command","offset":0,"length":5}]}}
+	return func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "Welcome to AskMeAnything")
+	}
+}
+
+func MessageHandler(bot *telegram.Bot) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		body, err := ioutil.ReadAll(r.Body)
+		if err != nil {
+			fmt.Println("err: ", err)
+		}
+		message := &telegram.Message{}
+		err = json.Unmarshal(body, r)
+		if err != nil {
+			fmt.Println("err: ", err)
+		}
+
+		bot.Respond(message)
+	}
 }
 
 func main() {
-	router := httprouter.New()
-	router.GET("/", indexHandler)
-	router.POST("/message", messageHandler)
-
-	env := os.Getenv("APP_ENV")
-	if env == "production" {
-		log.Println("Running api server in production mode")
-	} else {
-		log.Println("Running api server in dev mode")
+	bot := &telegram.Bot{
+		URL: BASEURL + APIKEY,
 	}
 
-	http.ListenAndServe(":"+os.Getenv("PORT"), router)
+	r := mux.NewRouter()
+	// Routes consist of a path and a handler function.
+	r.HandleFunc("/", IndexHandler()).Methods("GET")
+	r.HandleFunc("/message", MessageHandler(bot)).Methods("POST")
+
+	// Bind to a port and pass our router in
+	log.Fatal(http.ListenAndServe(":"+os.Getenv("PORT"), r))
 }
---
+++ telegram/telegram.go
@@ -0,0 +1,50 @@
+package telegram
+
+import (
+	"fmt"
+	"net/http"
+	"strconv"
+)
+
+const (
+	SEND = "/sendmessage"
+)
+
+type Message struct {
+	Id   int    `json:"message_id"`
+	From User   `json:"from"`
+	Chat Chat   `json:"chat"`
+	Date int    `json:"date"`
+	Text string `json:"text"`
+}
+
+type User struct {
+	FirstName  string `json:"first_name"`
+	SecondName string `json:"second_name"`
+}
+
+type Chat struct {
+	Id   int    `json:"id"`
+	Type string `json:"type"`
+}
+
+type Bot struct {
+	URL string
+}
+
+func (b *Bot) Respond(message *Message) {
+	chatId := strconv.Itoa(message.Chat.Id)
+	msg := "Hello " + message.From.FirstName + "reply for " + message.Text
+
+	url := b.URL + SEND + "?chat_id=" + chatId + "&text=" + msg
+	req, err := http.NewRequest("GET", url, nil)
+	if err != nil {
+		fmt.Println("err", err)
+	}
+
+	client := &http.Client{}
+	_, e := client.Do(req)
+	if e != nil {
+		fmt.Println("e", e, url)
+	}
+}
