71cab3e47b772baad115a9ec7e117f0785bd41cc
Abhishek Pradhan <Abhishek_Pradhan@external.mckinsey.com>
2017-05-03 15:09:20 +0530

    Add dummy handler for recieving message

--- main.go
+++ main.go
@@ -12,9 +12,16 @@ func indexHandler(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
 	fmt.Fprintf(w, "Welcome to AskMeAnything")
 }
 
+func messageHandler(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
+	//"message":{"message_id":271,"from":{"id":35364348,"first_name":"Abhishek","last_name":"Pradhan"},
+	// "chat":{"id":35364348,"first_name":"Abhishek","last_name":"Pradhan","type":"private"},
+	// "date":1493803385,"text":"/ping","entities":[{"type":"bot_command","offset":0,"length":5}]}}
+}
+
 func main() {
 	router := httprouter.New()
 	router.GET("/", indexHandler)
+	router.POST("/message", messageHandler)
 
 	env := os.Getenv("APP_ENV")
 	if env == "production" {
